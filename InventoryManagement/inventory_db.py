426d0d8fa0655d9197474d8096eb01ba3506b950
250mon <lambki@naver.com>
2022-04-08 08:16:21 +0900

    .

--- InventoryManagement/inventory_db.py
+++ InventoryManagement/inventory_db.py
@@ -3,215 +3,199 @@ from sqlite3 import Error
 from datetime import date
 
 
-def create_connection(db_file):
-    """
-    Create a database connection to the SQLite database
-    specified by db_file
-    :param db_file:
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
+class InventoryDB:
+    def __init__(self, db_file):
+        self.connection = self._create_connection(db_file)
+        self._create_inventory_tables()
 
-    return conn
+    def _create_connection(self, db_file):
+        """
+        Create a database connection to the SQLite database
+        specified by db_file
+        :param db_file:
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
 
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+        return conn
 
-def create_inventory_tables(db_file):
-    sql_create_items_table = """ CREATE TABLE IF NOT EXISTS items (
+    def _create_table(self, create_table_sql):
+        """ create a table from the create_table_sql statement
+        :param conn: Connection object
+        :param create_table_sql: a CREATE TABLE statement
+        :return:
+        """
+        try:
+            c = self.connection.cursor()
+            c.execute(create_table_sql)
+        except Error as e:
+            print(e)
+
+    def _create_inventory_tables(self):
+        sql_create_items_table = """ CREATE TABLE IF NOT EXISTS items (
+                                            id integer PRIMARY KEY,
+                                            code text NOT NULL,
+                                            name text NOT NULL
+                                        ); """
+
+        sql_create_transactions_table = """CREATE TABLE IF NOT EXISTS transactions (
                                         id integer PRIMARY KEY,
-                                        code text NOT NULL,
-                                        name text NOT NULL
-                                    ); """
+                                        item_code text NOT NULL,
+                                        category text NOT NULL,
+                                        quantity integer NOT NULL,
+                                        inventory integer NOT NULL,
+                                        date text NOT NULL,
+                                        FOREIGN KEY (item_code) REFERENCES items (code)
+                                    );"""
 
-    sql_create_transactions_table = """CREATE TABLE IF NOT EXISTS transactions (
-                                    id integer PRIMARY KEY,
-                                    item_code text NOT NULL,
-                                    category text NOT NULL,
-                                    quantity integer NOT NULL,
-                                    inventory integer NOT NULL,
-                                    date text NOT NULL,
-                                    FOREIGN KEY (item_code) REFERENCES items (code)
-                                );"""
+        # create tables
+        if self.connection is not None:
+            # create items table
+            self._create_table(sql_create_items_table)
 
-    # create a database connection
-    conn = create_connection(db_file)
+            # create transactions table
+            self._create_table(sql_create_transactions_table)
+        else:
+            print("Error! cannot create the database self.connection.")
 
-    # create tables
-    if conn is not None:
-        # create items table
-        create_table(conn, sql_create_items_table)
+    def _execute_sql(self, *sql):
+        print(sql)
+        cursor = self.connection.cursor()
+        cursor.execute(*sql)
+        self.connection.commit()
+        return cursor
 
-        # create transactions table
-        create_table(conn, sql_create_transactions_table)
-    else:
-        print("Error! cannot create the database connection.")
-    conn.close()
+    def create_item(self, item):
+        """
+        Create a new item into the items table
+        :param item:
+        :return: item id
+        """
+        sql = ''' INSERT INTO items(code,name)
+                  VALUES(?,?)'''
+        cur = self._execute_sql(sql, item)
+        return cur.lastrowid
 
-def create_item(conn, item):
-    """
-    Create a new item into the items table
-    :param conn:
-    :param item:
-    :return: item id
-    """
-    sql = ''' INSERT INTO items(code,name)
-              VALUES(?,?)'''
-    cur = conn.cursor()
-    cur.execute(sql, item)
-    conn.commit()
-    return cur.lastrowid
+    def create_transaction(self, transaction):
+        """
+        Create a new transaction
+        :param transaction:
+        :return: transaction id
+        """
+        sql = ''' INSERT INTO transactions(item_code,category,quantity,inventory,date)
+                  VALUES(?,?,?,?,?) '''
+        cur = self._execute_sql(sql, transaction)
+        return cur.lastrowid
 
-def create_transaction(conn, transaction):
-    """
-    Create a new transaction
-    :param conn:
-    :param transaction:
-    :return: transaction id
-    """
-    sql = ''' INSERT INTO transactions(item_code,category,quantity,inventory,date)
-              VALUES(?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.execute(sql, transaction)
-    conn.commit()
-    return cur.lastrowid
+    def update_transaction(self, transaction):
+        """
+        Update(Modify) the transaction category, quantity, inventory, date
+        :param transaction:
+        :return:
+        """
+        sql = ''' UPDATE transactions
+                  SET category = ?
+                  SET quantity = ?
+                  SET inventory = ?
+                  SET date = ?
+                  WHERE id = ? '''
+        self._execute_sql(sql, transaction)
 
-def update_transaction(conn, transaction):
-    """
-    Update(Modify) the transaction category, quantity, inventory, date
-    :param conn:
-    :param transaction:
-    :return:
-    """
-    sql = ''' UPDATE transactions
-              SET category = ?
-              SET quantity = ?
-              SET inventory = ?
-              SET date = ?
-              WHERE id = ? '''
-    cur = conn.cursor()
-    cur.execute(sql, transaction)
-    conn.commit()
+    def delete_item(self, id):
+        """
+        Delete a transaction by item id
+        :param id:
+        :return:
+        """
+        sql = 'DELETE FROM items WHERE id=?'
+        self._execute_sql(sql, (id,))
 
-def delete_item(conn, id):
-    """
-    Delete a transaction by item id
-    :param conn:
-    :param id:
-    :return:
-    """
-    sql = 'DELETE FROM items WHERE id=?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
+    def delete_all_items(self):
+        """
+        Delete all rows in the items table
+        :param
+        :return:
+        """
+        sql = 'DELETE FROM items'
+        self._execute_sql(sql)
 
-def delete_all_items(conn):
-    """
-    Delete all rows in the items table
-    :param conn: Connection to the SQLite database
-    :return:
-    """
-    sql = 'DELETE FROM items'
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+    def delete_transaction(self, id):
+        """
+        Delete a transaction by transaction id
+        :param id:
+        :return:
+        """
+        sql = 'DELETE FROM transactions WHERE id=?'
+        self._execute_sql(sql, (id,))
 
-def delete_transaction(conn, id):
-    """
-    Delete a transaction by transaction id
-    :param conn:
-    :param id:
-    :return:
-    """
-    sql = 'DELETE FROM transactions WHERE id=?'
-    cur = conn.cursor()
-    cur.execute(sql, (id,))
-    conn.commit()
+    def delete_all_transactions(self):
+        """
+        Delete all rows in the transactions table
+        :param
+        :return:
+        """
+        sql = 'DELETE FROM transactions'
+        self._execute_sql(sql)
 
-def delete_all_transactions(conn):
-    """
-    Delete all rows in the transactions table
-    :param conn: Connection to the SQLite database
-    :return:
-    """
-    sql = 'DELETE FROM transactions'
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
+    def select_all_items(self):
+        """
+        Query all rows in the items table
+        :param
+        :return:
+        """
+        sql = "SELECT * FROM items"
+        cur = self._execute_sql(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-def select_all_items(conn):
-    """
-    Query all rows in the items table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM items")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    def select_item_by_code(self, code):
+        """
+        Query items by code
+        :param priority:
+        :return:
+        """
+        sql = ("SELECT * FROM items WHERE item_code=?", (code,))
+        cur = self._execute_sql(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-def select_item_by_code(conn, code):
-    """
-    Query items by code
-    :param conn: the Connection object
-    :param priority:
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM items WHERE item_code=?", (code,))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    def select_all_transactions(self):
+        """
+        Query all rows in the transactions table
+        :param
+        :return:
+        """
+        sql = "SELECT * FROM transactions"
+        cur = self._execute_sql(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
-def select_all_transactions(conn):
-    """
-    Query all rows in the transactions table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM transactions")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-def select_transaction_by_code(conn, code):
-    """
-    Query transactions by code
-    :param conn: the Connection object
-    :param priority:
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM transactions WHERE item_code=?", (code,))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    def select_transaction_by_code(self, code):
+        """
+        Query transactions by code
+        :param code:
+        :return:
+        """
+        sql = ("SELECT * FROM transactions WHERE item_code=?", (code,))
+        cur = self._execute_sql(sql)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
 
 if __name__ == '__main__':
-    database = 'test_inventory.db'
+    inv_db = InventoryDB('test_inventory.db')
 
-    create_inventory_tables(database)
-
-    conn = create_connection(database)
-    with conn:
+    with inv_db.connection:
         items = [('AA', 'band'), ('BB', 'needle')]
         for item in items:
-            create_item(conn, item)
+            inv_db.create_item(item)
 
         transactions = [
             ('AA', 'buy', 10, 10, date.today().isoformat()),
@@ -220,11 +204,11 @@ if __name__ == '__main__':
             ('CC', 'buy', 1, 11, date.today().isoformat()),
             ]
         for transaction in transactions:
-            create_transaction(conn, transaction)
+            inv_db.create_transaction(transaction)
 
-        select_all_items(conn)
-        select_transaction_by_code(conn, 'DD')
-        select_all_transactions(conn)
+        inv_db.select_all_items()
+        inv_db.select_transaction_by_code('DD')
+        inv_db.select_all_transactions()
 
-        delete_all_items(conn)
-        delete_all_transactions(conn)
+        inv_db.delete_all_items()
+        inv_db.delete_all_transactions()
