f8deaa4ae1e867a7347d41ca818e5dc26d8c44f8
Jyeyoung Song <lambki@naver.com>
2022-05-19 21:19:14 +0900

    .

--- EmailSender/sending_attachments.py
+++ EmailSender/sending_attachments.py
@@ -3,6 +3,7 @@ import os
 
 from email import encoders
 from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -31,19 +32,17 @@ file_path = os.path.join(dir_path, file_name)
 
 # Open PDF file in binary mode
 with open(file_path, "rb") as attachment:
-    # Add file as apllication/octet-stream
-    # Email client can usually download this automaticaaly as attachment
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
     part = MIMEBase("application", "octet-stream")
     part.set_payload(attachment.read())
+    # part = MIMEApplication(attachment.read(), _subtype="pdf")
 
 # Encode file in ASCII characters to attachment part
 encoders.encode_base64(part)
 
 # Add header as key/value pair to attachment part
-part.add_header(
-    "Content-Disposition",
-    f"attachment; filename= {file_path}",
-)
+part.add_header("Content-Disposition", 'attachment', filename=str(file_path))
 
 # Add attachment to message and convert message to string
 message.attach(part)
@@ -61,6 +60,7 @@ try:
     server.login(sender_email, password)
     # Send email
     server.sendmail(sender_email, receiver_email, text)
+    # server.send_message(message)
 except Exception as e:
     # Print any error messages to stdout
     print(e)
